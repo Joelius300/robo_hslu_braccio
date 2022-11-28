e5e969d8b9e5e668069cd04631517a907800555f
Joel L <joelius300@protonmail.com>
2022-11-28 11:31:26 +0100

    Playground stuff

--- joel-playground.py
+++ joel-playground.py
@@ -1,16 +1,37 @@
 import time
 
-import serial
-
 from braccio import Braccio
 
 if __name__ == '__main__':
-    port = "/dev/ttyACM2"
+    port = "/dev/ttyACM0"
     # port = serial.serial_for_url("loop://", timeout=.1)  # testing
     with Braccio(port) as b:
-        time.sleep(2)
+        time.sleep(5)  # important
 
-        # sent = b.send(base=90, shoulder=90, elbow=90, wrist_tilt=90, wrist_rotate=90, grip=40)  # default position
-        # sent = b.send(base=120, shoulder=120, elbow=120, wrist_tilt=120, wrist_rotate=120, grip=40)
-        sent = b.send(base=60, shoulder=60, elbow=60, wrist_tilt=60, wrist_rotate=60, grip=40)
-        print("Sent no. bytes:", sent)
+        b.reset_position()
+
+        time.sleep(10)
+        b.reset_position()
+
+
+def hard_coded_pick_and_place(b: Braccio):
+    """DON'T RUN THIS, this was before translations."""
+    # 120 b 75 s 20 e 20 v 90 w 0 g
+    # 55 g
+    # 90 s
+    # 75 b
+    # 75 s
+    # 0 g
+    return None
+
+    b.send(base=120, shoulder=75, elbow=20, wrist_tilt=20, wrist_rotate=90, grip=100)
+    time.sleep(5)
+    b.send(grip=20)
+    time.sleep(2)
+    b.send(shoulder=90)
+    time.sleep(2)
+    b.send(base=75)
+    time.sleep(2)
+    b.send(shoulder=75)
+    time.sleep(2)
+    b.send(grip=100)
