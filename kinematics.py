aca12369d31ad97efcf01492c13694990f038ac2
Joel L <joelius300@protonmail.com>
2022-11-29 08:55:32 +0100

    nitpicks

--- kinematics.py
+++ kinematics.py
@@ -7,39 +7,39 @@ import numpy as np
 
 
 def rot(axis: Literal['x', 'y', 'z'], angle: int | float):
-    """Create a rotation matrix for angle around axis"""
+    """Create a rotation matrix for angle around axis."""
 
     t = angle * np.pi / 180
     c = cos(t)
     s = sin(t)
-    m = np.array()
 
     if axis == 'x':
-        m = np.array([
+        return np.array([
             [1, 0, 0, 0],
             [0, c, -s, 0],
             [0, s, c, 0],
             [0, 0, 0, 1]
         ])
     elif axis == 'y':
-        m = np.array([
+        return np.array([
             [c, 0, s, 0],
             [0, 1, 0, 0],
             [-s, 0, c, 0],
             [0, 0, 0, 1]
         ])
-    else:
-        m = np.array([
+    elif axis == 'z':
+        return np.array([
             [c, -s, 0, 0],
             [s, c, 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ])
-    return m
+    else:
+        raise ValueError(f"Axis can only be x, y or z! not {axis}")
 
 
 def trans(x=0, y=0, z=0):
-    """Create a translation matrix for x, y, z"""
+    """Create a translation matrix for x, y, z."""
     return np.array([
         [1, 0, 0, x],
         [0, 1, 0, y],
